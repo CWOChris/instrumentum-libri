0a85badc169493671892ba9c3111681a06bd55ae
Chris R <136107779+CWOChris@users.noreply.github.com>
2024-02-27 14:38:27 -0600

    added additional components to the App.jsx file

--- Develop/client/src/App.jsx
+++ Develop/client/src/App.jsx
@@ -1,14 +1,34 @@
 import './App.css';
-import { Outlet } from 'react-router-dom';
-
+import { ApolloProvider } from '@apollo/client';
+import { BrowserRouter as Router, Route, Switch } from'react-router-dom';
+import { client } from './utils/client';
 import Navbar from './components/Navbar';
+import SearchBooks from './pages/SearchBooks';
+import SavedBooks from './pages/SavedBooks';
+import LoginForm from './components/LoginForm';
+import SignUpForm from './components/SignUpForm';
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Outlet />
-    </>
+    <ApolloProvider client={client}>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path='/'>
+            <SearchBooks />
+          </Route>
+          <Route exact path='/saved'>
+            <SavedBooks />
+          </Route>
+          <Route exact path='/login'>
+            <LoginForm />
+          </Route>
+          <Route exact path='/signup'>
+            <SignUpForm />
+          </Route>
+        </Switch>
+      </Router>
+    </ApolloProvider>
   );
 }
 
