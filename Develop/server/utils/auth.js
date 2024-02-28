9e2e42b2809e01ccc554f7e56c83b816f640f2ad
Chris R <136107779+CWOChris@users.noreply.github.com>
2024-02-27 22:23:30 -0600

    cleaned up the auth.js server

--- Develop/server/utils/auth.js
+++ Develop/server/utils/auth.js
@@ -5,12 +5,8 @@ const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
-  // function for our authenticated routes
-  authMiddleware: function ({ req }) {
-    // allows token to be sent via  req.query or headers
+  authMiddleware: function (req, res, next) {
     let token = req.body.token || req.query.token || req.headers.authorization;
-
-    // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
       token = token.split(' ').pop().trim();
     }
@@ -19,18 +15,16 @@ module.exports = {
       return res.status(400).json({ message: 'You have no token!' });
     }
 
-    // verify token and get user data out of it
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
-    } catch {
-      console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
+      next();
+    } catch (error) {
+      console.log(error, "invalid token");
+      return res.status(400).json({ message: 'Invalid token'});
     }
-
-    // send to next endpoint
-    next();
   },
+
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
 
